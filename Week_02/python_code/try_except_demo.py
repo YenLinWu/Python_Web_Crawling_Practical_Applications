2a6b468d61325c0139c57d4adf8f842fc1701e4b
LAPTOP-RQOICGCV\User <yenlinwu1112@gmail.com>
2026-09-22 18:31:24 +0800

    範例程式碼

---
+++ Week_02/python_code/try_except_demo.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+x = 0
+y = 100/x
+
+if y > 50:
+    print("y > 50")
+else:
+    print("y <= 50")
+
+
+# try:
+#     x = 0
+#     y = 100 / x
+#     if y > 50:
+#         print("y > 50")
+#     else:
+#         print("y <= 50")
+
+# except ZeroDivisionError:
+#     print("x 不能為 0")
